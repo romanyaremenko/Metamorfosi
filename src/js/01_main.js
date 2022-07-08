15c008aff2ee2f441b2cef2462345b70fdd638b6
Roman Yaremenko <romanyaremenko95@gmail.com>
2022-07-08 22:02:58 +0300

    note

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,4 +1,32 @@
-let partnersBlock = document.querySelector('.partners')
-if(partnersBlock) {
 
+let partnersBlock = document.querySelector('.partners')
+
+if(partnersBlock) {
+    let logoTab = document.querySelectorAll(".logo-item")
+    let reviewTab = document.querySelectorAll('.review-item')
+
+    logoTab.forEach((itemLogo,idxLogo)=> {
+        itemLogo.addEventListener('click',function (){
+            logoTab.forEach((item)=> {
+                item.classList.remove('active')
+            })
+
+            reviewTab.forEach((itemReview,idxReview)=> {
+
+                if(idxLogo === idxReview) {
+                    itemReview.classList.add('active')
+                    itemLogo.classList.add('active')
+                } else {
+                    itemReview.classList.remove('active')
+                }
+            })
+        })
+    })
 }
+
+var swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+});
